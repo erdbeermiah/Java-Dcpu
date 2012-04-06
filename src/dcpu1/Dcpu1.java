3b4d38dc8a73c005183a1bdab1d6c7c16b533c3a
Björn Aigner <bjoern.aigner@innogames.de>
2012-04-07 00:03:19 +0200

    Simplified code Removed unused vars Beautified code ... stuff

--- src/dcpu1/Dcpu1.java
+++ src/dcpu1/Dcpu1.java
@@ -18,9 +18,9 @@ public class Dcpu {
 	private static final int NUM_REGISTERS = 8;
 	
 	// ram positions
-	private static final int PC  = 0;
-	private static final int SP  = 1;
-	private static final int OV  = 2;
+	private static final int PC = 0;
+	private static final int SP = 1;
+	private static final int OV = 2;
 	private static final int REG = 3;
 	private static final int LIT = 4;
 	private static final int MEM = 5;
@@ -34,15 +34,15 @@ public class Dcpu {
 	 *		4 - literals
 	 *		5 - memory
 	 */
-	private int[][] ram = new int[6][];
-	private int     rp  = MEM; // ram position
+	private int[][] ram  = new int[6][];
+	private int     rp   = MEM; // ram position
 	
-	private boolean skipNextWord = false;
+	private boolean skip = false;
 	
 	public Dcpu (int[] program) {
-		ram[PC]  = new int[1];
-		ram[SP]  = new int[1];
-		ram[OV]  = new int[1];
+		ram[PC] = new int[1];
+		ram[SP] = new int[1];
+		ram[OV] = new int[1];
 		ram[REG] = new int[NUM_REGISTERS];
 		ram[LIT] = new int[NUM_ITERALS];
 		ram[MEM] = new int[MEMORY_SIZE];
@@ -56,7 +56,7 @@ public class Dcpu {
 		}
 	}
 	
-	private int parseVar (int code) {
+	private int operate (int code) {
 		if (code < 0x08) {
 			rp = REG;
 			return code;
@@ -76,20 +76,19 @@ public class Dcpu {
 			return --ram[SP][0];
 		} else if (code == 0x1B) {
 			rp = SP;
-			return 0;
+			return ram[SP][0];
 		} else if (code == 0x1C) {
 			rp = PC;
-			return 0;
+			return ram[PC][0];
 		} else if (code == 0x1D) {
 			rp = OV;
-			return 0;
+			return ram[OV][0];
 		} else if (code == 0x1E) {
 			rp = MEM;
 			return ram[PC][0]++;
 		} else if (code == 0x1F) {
 			rp = PC;
-			ram[PC][0]++;
-			return 0;
+			return ram[PC][0]++;
 		}
 		
 		return (code - 0x20) % NUM_ITERALS;
@@ -108,9 +107,9 @@ public class Dcpu {
 		if (op == 0) {
 			switch (a) {
 				case 0x01:
-					a = parseVar(b);
-					if (skipNextWord) {
-						skipNextWord = false;
+					a = operate(b);
+					if (skip) {
+						skip = false;
 					} else {
 						ram[MEM][--ram[SP][0]] = ram[PC][0];
 						ram[PC][0] = a;
@@ -121,42 +120,48 @@ public class Dcpu {
 					System.exit(1);
 			}
 		} else {
+			int res = 0, dst = a;
 			int[] w, r;
+			int wPos, rPos;
 			
-			a = parseVar(a);
+			a = operate(a);
+			wPos = (rp > OV ? a : 0);
 			w = ram[rp];
 			
-			b = parseVar(b);
+			b = operate(b);
+			rPos = (rp > OV ? b : 0);
 			r = ram[rp];
 
-			if (skipNextWord) {
-				skipNextWord = false;
+			if (skip) {
+				skip = false;
 				return true;
 			}
 
 			switch (op) {
-				case 0x1: w[a]  = r[b];                                       break;
-				case 0x2: w[a] += r[b];                                       break;
-				case 0x3: w[a] -= r[b];                                       break;
-				case 0x4: w[a] *= r[b];                                       break;
-				case 0x5: if (r[b] == 0) { w[a] = 0; } else { w[a] /= r[b]; } break;
-				case 0x6: if (r[b] == 0) { w[a] = 0; } else { w[a] &= r[b]; } break;
-				case 0x7: w[a]  <<= r[b];                                     break;
-				case 0x8: w[a] >>>= r[b];                                     break;
-				case 0x9: w[a]   &= r[b];                                     break;
-				case 0xA: w[a]   |= r[b];                                     break;
-				case 0xB: w[a]   ^= r[b];                                     break;
-				case 0xC: skipNextWord =  !(w[a] == r[b]);                    break;
-				case 0xD: skipNextWord =  !(w[a] != r[b]);                    break;
-				case 0xE: skipNextWord =  !(w[a] >  r[b]);                    break;
-				case 0xF: skipNextWord = !((w[a] &  r[b]) != 0);              break;
+				case 0x1: w[wPos] = r[rPos]; break;
+				case 0x2: w[wPos] += r[rPos]; break;
+				case 0x3: w[wPos] -= r[rPos]; break;
+				case 0x4: w[wPos] *= r[rPos]; break;
+				case 0x5: if (r[rPos] == 0) { w[wPos] = 0; } else { w[wPos] /= r[rPos]; } break;
+				case 0x6: if (r[rPos] == 0) { w[wPos] = 0; } else { w[wPos] &= r[rPos]; } break;
+				case 0x7: w[wPos] <<= r[rPos];  break;
+				case 0x8: w[wPos] >>>= r[rPos]; break;
+				case 0x9: w[wPos] &= r[rPos];   break;
+				case 0xA: w[wPos] |= r[rPos];   break;
+				case 0xB: w[wPos] ^= r[rPos];   break;
+				case 0xC: skip =  !(w[wPos] == r[rPos]); break;
+				case 0xD: skip =  !(w[wPos] != r[rPos]); break;
+				case 0xE: skip =  !(w[wPos] >  r[rPos]);      break;
+				case 0xF: skip = !((w[wPos] & r[rPos]) != 0); break;
 				default:
 					System.err.println("0xFFFF: Illegal operation! ("+ Integer.toHexString(op) +")");
 					System.exit(1);
 			}
 			
-			if (op > 0x1 && op < 0x9 && op != 0x6) {
-				ram[OV][0] = (w[a] >> SHORT_SHIFT) & SHORT_MASK;
+			switch (op) {
+				case 0x2: case 0x3: case 0x4: case 0x5: case 0x7: case 0x8:
+					ram[OV][0] = (w[wPos] >> SHORT_SHIFT) & SHORT_MASK;
+					break;
 			}
 		}
 		
@@ -169,20 +174,20 @@ public class Dcpu {
 	
 	@Override
 	public String toString () {
-		return                                                   "| "+
-		       hex(ram[PC][0])                                 +" | "+
-		       hex(ram[SP][0])                                 +" | "+
-		       hex(ram[OV][0])                                 +" | "+
-		       hex(skipNextWord ? 1 : 0)                       +" | "+
-		       hex(ram[REG][0])                                +" | "+
-		       hex(ram[REG][1])                                +" | "+
-		       hex(ram[REG][2])                                +" | "+
-		       hex(ram[REG][3])                                +" | "+
-		       hex(ram[REG][4])                                +" | "+
-		       hex(ram[REG][5])                                +" | "+
-		       hex(ram[REG][6])                                +" | "+
-		       hex(ram[REG][7])                               +" ||| "+
-		       hex((ram[MEM][ram[PC][0]] & OP_MASK))           +" | "+
+		return "| "+
+		       hex(ram[PC][0])   +" | "+
+		       hex(ram[SP][0])   +" | "+
+		       hex(ram[OV][0])   +" | "+
+		       hex(skip ? 1 : 0) +" | "+
+		       hex(ram[REG][0])  +" | "+
+		       hex(ram[REG][1])  +" | "+
+		       hex(ram[REG][2])  +" | "+
+		       hex(ram[REG][3])  +" | "+
+		       hex(ram[REG][4])  +" | "+
+		       hex(ram[REG][5])  +" | "+
+		       hex(ram[REG][6])  +" | "+
+		       hex(ram[REG][7])  +" ||| "+
+		       hex((ram[MEM][ram[PC][0]] & OP_MASK)) +" | "+
 		       hex((ram[MEM][ram[PC][0]] >> A_SHIFT) & A_MASK) +" | "+
 		       hex((ram[MEM][ram[PC][0]] >> B_SHIFT) & B_MASK) +" |";
 	}
@@ -190,19 +195,25 @@ public class Dcpu {
 	public static void main(String[] args) {
 		Dcpu cpu = new Dcpu(new int[] {
 			// schreibe 0x1 in register A (0x0) (increment)
-			word(0x1, 0x0, 0x1E), 0x1,
+			w(0x1, 0x0, 0x1E), 0x1,
+
 			// schreibe 0x4 in register B (0x1) (jump lable)
-			word(0x1, 0x1, 0x1E), 0x6,
-			// schreibe 0x2 in register C (0x2) (ziel wert)
-			word(0x1, 0x2, 0x1E), 0x2,
+			w(0x1, 0x1, 0x1E), 0x6,
+
+			// schreibe 0x4 in register C (0x2) (ziel wert)
+			w(0x1, 0x2, 0x1E), 0x2,
+
 			// addiere zu register J (0x7) den wert aus register A (0x0)
-			word(0x2, 0x7, 0x0), // <- index 0x6 :)
+			w(0x2, 0x7, 0x0), // <- index 0x6 :)
+
 			// wenn J (0x7) ungleich C (0x2)
 			//		führe nächste zeile aus
 			//		sonst übernächste
-			word(0xD, 0x7, 0x2),
+			w(0xD, 0x7, 0x2),
+
 			// setze PC (0x1C) auf wert aus register B (0x1)
-			word(0x1, 0x1C, 0x1),
+			w(0x1, 0x1C, 0x1),
+
 			0xFFFF0000, // exit
 		});
 		
@@ -213,7 +224,7 @@ public class Dcpu {
 		}
 	}
 	
-	static int word (int op, int a, int b) {
+	static int w (int op, int a, int b) {
 		return op + (a << 4) + (b << 10);
 	}
 	
